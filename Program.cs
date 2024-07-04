5dcfd4f897a3742fb391c45d1bef57cb0aa6331b
hcslearning <learning@hardcybersoft.cl>
2024-07-04 14:16:57 -0400

    codigo que usa dependencia JSON

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+namespace Nuget.Quickstart {
+    public class Usuario {
+        public string? Nombre {get; set;}
+        public string? Email {get; set;}
+        public DateTime? Fecha {get; set;}
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+            Usuario usuario = new Usuario {
+                Nombre = "Juan",
+                Email = "juan@123.cl",
+                Fecha = new DateTime(1980, 12, 25, 0, 0, 0, DateTimeKind.Utc)
+            };
+            string json = JsonConvert.SerializeObject(usuario, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
